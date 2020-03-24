3e8982e9eb30864c7528a926571e882e8d13e4b8
Zireael07 <zi@mail.com>
2020-03-24 19:14:57 +0100

    new: move the @

--- source/app.d
+++ source/app.d
@@ -14,13 +14,40 @@ void main()
     int nb_char_horiz = 0, nb_char_vertic = 0;
     TCOD_console_set_custom_font(toStringz(font), font_flags, nb_char_horiz, nb_char_vertic);
     
+    //centrally on map
+    int playerx=40,playery=25;
+
     //hello world
     TCOD_console_init_root(80, 50, "Hello, world.", false, TCOD_RENDERER_SDL);
 
+    //Game loop
+    //1. input
+    //2. draw
     while(!TCOD_console_is_window_closed()) {
-        TCOD_console_print(null, 0, 0, "Hello, world.");
-        TCOD_console_flush();
         auto k = TCOD_console_check_for_keypress(TCOD_KEY_PRESSED);
+        switch(k.vk) {
+            case TCODK_UP : 
+                playery--; 
+                break;
+            case TCODK_DOWN : 
+                playery++; 
+                break;
+            case TCODK_LEFT : 
+                playerx--; 
+                break;
+            case TCODK_RIGHT :
+                playerx++; 
+                break;
+            default:break;
+       }
+
+        
+        TCOD_console_clear(null);
+        TCOD_console_put_char(null, playerx, playery, '@', TCOD_BKGND_NONE);
+        
+        //TCOD_console_print(null, 0, 0, "Hello, world.");
+        TCOD_console_flush();
+        
     }
 }
 
