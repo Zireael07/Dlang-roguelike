99b241cca5d842ab86789279b86fe7d2fbbee0ea
Zireael07 <zi@mail.com>
2020-04-25 10:33:05 +0200

    new: add walls around the map

--- source/map.d
+++ source/map.d
@@ -20,6 +20,19 @@ class Map {
        //mapgen
        setWall(30,22);
        setWall(50,22);
+       
+       //walls around
+       foreach( y; 0 .. this.height )
+        {
+            setWall(0, y);
+            setWall(this.width-1, y);
+        }
+
+        foreach( x; 0 .. this.width )
+        {
+            setWall(x, 0);
+            setWall(x, this.height-1);
+        }
    }
 
    bool isWall(int x, int y){
